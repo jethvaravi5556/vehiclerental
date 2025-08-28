4bbd0ecfd412e7953f3e44bdbfc4caff2948bff3
Ravi Jethva <ravijethva2004@gmail.com>
2025-08-29 01:21:51 +0530

    some changes for deployment

--- backend/middlewares/session.js
+++ backend/middlewares/session.js
@@ -1,14 +1,19 @@
 // === backend/middleware/session.js ===
 import session from "express-session";
-
+import MongoStore from "connect-mongo";
 const sessionMiddleware = session({
   secret: process.env.SESSION_SECRET || "keyboardcat",
   resave: false,
   saveUninitialized: false,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGO_URI,
+    collectionName: "sessions",
+  }),
   cookie: {
+    secure: process.env.NODE_ENV === "production",
     httpOnly: true,
-    secure: false,
-    sameSite: "lax",
+    sameSite: "none",
+    maxAge:  7 * 24 * 60 * 60 * 1000,
   },
 });
 
